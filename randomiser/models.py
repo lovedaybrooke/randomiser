9545613dddafac4c6e319ac941de61f699e6bf62
Kat Matfield <katmatfield@gmail.com>
2021-01-31 20:47:08 +0100

    Show basic cards

--- randomiser/models.py
+++ randomiser/models.py
@@ -1,5 +1,7 @@
 import datetime
 
+from sqlalchemy import func
+
 from randomiser import db
 
 
@@ -9,3 +11,26 @@ class Card(db.Model):
     german = db.Column(db.String())
     part_of_speech = db.Column(db.String())
     last_viewed = db.Column(db.DateTime, nullable=True)
+
+    @classmethod
+    def pull(cls):
+        unseen_cards = Card.query.filter_by(last_viewed=None)
+        verb = unseen_cards.filter_by(part_of_speech="verb").order_by(
+               func.random()).first()
+        noun = unseen_cards.filter_by(part_of_speech="noun").order_by(
+               func.random()).first()
+        adj = unseen_cards.filter_by(part_of_speech="adjective/adverb"
+              ).order_by(func.random()).first()
+        extra = unseen_cards.filter(Card.part_of_speech != 'verb').order_by(
+                func.random()).first()
+        word_list = [verb, noun, adj, extra]
+        cls.update_dates(word_list)
+        return word_list
+
+    @classmethod
+    def update_dates(cls, items):
+        for item in items:
+            item.last_viewed = datetime.datetime.today()
+            db.session.add(item)
+        db.session.commit()
+
--- randomiser/routes.py
+++ randomiser/routes.py
@@ -1,5 +1,8 @@
+from flask import render_template
+
 from randomiser import app
+from randomiser.models import Card
 
 @app.route('/')
 def unfinished():
-	return("Hallo world")
+    return render_template('home.html', word_list = Card.pull())
